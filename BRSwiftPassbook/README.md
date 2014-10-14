f76dcde5333b540fea678d5c9cb06190bf76b0f9
Aaron Stephenson <aaron@bronron.com>
2014-10-14 18:40:03 +1100

    Added comments and updated Readme file

---
+++ BRSwiftPassbook/README.md
@@ -0,0 +1,15 @@
+BRSwiftPassbook
+===============
+
+Passbook with swift calls
+
+This repository was started to demo some of the Passkit calls but in Swift. The code is written in Swift. Feel free to have a look.
+
+Current Passkit methods used include
+-Adding a Pass to Passbook. Presenting PKAddPassesViewController.
+-Checking if a Pass is in Passbook.
+-Removing a sample Pass from Passbook.
+
+Unrelated to Passkit but in the project
+-Displaying an Alert to the user.
+-Accessing a file from the applications bundle.
--- BRSwiftPassbook/ViewController.swift
+++ BRSwiftPassbook/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController
     
     func passbookAvailable()->Bool
     {
+        //Checks if Passbook is available on the device, if not it displays a warning message.
         if (PKPassLibrary.isPassLibraryAvailable())
         {
             return true
@@ -44,16 +45,20 @@ class ViewController: UIViewController
 
     @IBAction func showPassbookPass(AnyObject)
     {
+        //Checks if Passbook is available on the device.
         if (passbookAvailable())
         {
             let path = NSBundle.mainBundle().pathForResource("pass900452108", ofType: "pkpass")
             let data = NSData(contentsOfFile: path!)
             var pass = PKPass(data: data, error:nil)
+            
+            //Checks if the pass has already been added to the users Passbook. If it is not then it adds it.
             if (!PKPassLibrary().containsPass(pass))
             {
                 var viewTmp = PKAddPassesViewController(pass: pass)
                 self.presentViewController(viewTmp, animated: true, completion: nil)
             }
+                //If the pass is already in the users passbook it asks the user if they wish to remove the pass.
             else
             {
                 var alert = UIAlertController(title: "Passbook", message: "This pass already in your Passbook.", preferredStyle: UIAlertControllerStyle.Alert)
@@ -72,12 +77,16 @@ class ViewController: UIViewController
     
     @IBAction func isPassAlreadyInPassbook()
     {
-        var alertString = "Pass not found in your Passbook."
+        //Checks if Passbook is available on the device.
         if (passbookAvailable())
         {
+            //Loads a demo pass saved in the applications bundle.
+            var alertString = "Pass not found in your Passbook."
             let path = NSBundle.mainBundle().pathForResource("pass900452108", ofType: "pkpass")
             let data = NSData(contentsOfFile: path!)
             var pass = PKPass(data: data, error:nil)
+            
+            //Checks if the pass has already been added to the users Passbook. If it has it changes the message to appear.
             if (PKPassLibrary().containsPass(pass))
             {
                 alertString = "You have already added this pass."
